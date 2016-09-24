05ec5a3e791d0d542e82a06174e814dfb89e9804
fransanchez <fransanchezgarcia@gmail.com>
2016-09-24 11:57:59 +0100

    adding application class

---
+++ app/src/main/java/takemewith/io/takemewith/TakemeWithApplication.java
@@ -0,0 +1,12 @@
+package takemewith.io.takemewith;
+
+import android.app.Application;
+
+public class TakemeWithApplication extends Application {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
