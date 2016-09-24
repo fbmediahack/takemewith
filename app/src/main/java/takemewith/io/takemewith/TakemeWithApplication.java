2869061ee64b09938f14750373d961d4ecff93b5
fransanchez <fransanchezgarcia@gmail.com>
2016-09-24 12:33:57 +0100

    adding beaconManager and beacons information

--- app/src/main/java/takemewith/io/takemewith/TakemeWithApplication.java
+++ app/src/main/java/takemewith/io/takemewith/TakemeWithApplication.java
@@ -2,11 +2,26 @@ package takemewith.io.takemewith;
 
 import android.app.Application;
 
+import com.estimote.sdk.BeaconManager;
+
 public class TakemeWithApplication extends Application {
 
+    private BeaconManager beaconManager;
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        beaconManager = new BeaconManager(getApplicationContext());
+
+        beaconManager.connect((new BeaconManager.ServiceReadyCallback() {
+            @Override
+            public void onServiceReady() {
+//                beaconManager.startMonitoring(new Region(
+//                        "monitored region",
+//                        UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"),
+//                        22504, 48827));
+            }
+        }));
     }
 }
