05229c56e2cccfb66cac7f5c549586a8adafcda2
fransanchez <fransanchezgarcia@gmail.com>
2016-09-24 14:42:39 +0100

    merging pink and beetroot beacons in one room

--- app/src/main/java/takemewith/io/takemewith/BeaconsData.java
+++ app/src/main/java/takemewith/io/takemewith/BeaconsData.java
@@ -12,21 +12,19 @@ public final class BeaconsData {
     public static final Integer TAG_BEACON_MAJOR = 6168;
     public static final Integer TAG_BEACON_MINOR = 32661;
 
-    public static final UUID HOUSE_BEACON_PINK =
-            UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE81");
+    public static final UUID HOUSE_BEACONS_UUID =
+            UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE01");
+
     public static final Integer HOUSE_BEACON_PINK_MAJOR = 3169;
     public static final Integer HOUSE_BEACON_PINK_MINOR = 41988;
 
-    public static final UUID HOUSE_BEACON_PURPLE =
-            UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE81");
+
     public static final Integer HOUSE_BEACON_PURPLE_MAJOR = 18713;
     public static final Integer HOUSE_BEACON_PURPLE_MINOR = 56498;
 
-    public static final Region ROOM1_REGION = new Region("Room1", HOUSE_BEACON_PINK,
-            HOUSE_BEACON_PINK_MAJOR, HOUSE_BEACON_PINK_MINOR);
+    public static final Region ROOM_REGION = new Region("House Room", HOUSE_BEACONS_UUID,
+            null, null);
 
-    public static final Region ROOM2_REGION = new Region("Room2", HOUSE_BEACON_PURPLE,
-            HOUSE_BEACON_PURPLE_MAJOR, HOUSE_BEACON_PURPLE_MINOR);
 
     public static final Region TAG_BEACON_REGION = new Region ("TagBeacon",
             TAG_BEACON, TAG_BEACON_MAJOR, TAG_BEACON_MINOR);
--- app/src/main/java/takemewith/io/takemewith/TakemeWithApplication.java
+++ app/src/main/java/takemewith/io/takemewith/TakemeWithApplication.java
@@ -1,11 +1,6 @@
 package takemewith.io.takemewith;
 
 import android.app.Application;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Intent;
-import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
 
 import com.estimote.sdk.Beacon;
@@ -31,9 +26,8 @@ public class TakemeWithApplication extends Application {
             @Override
             public void onServiceReady() {
                 // Start monitoring regions
-                beaconManager.startMonitoring(BeaconsData.ROOM1_REGION);
-                beaconManager.startMonitoring(BeaconsData.ROOM2_REGION);
                 beaconManager.startRanging(BeaconsData.TAG_BEACON_REGION);
+                beaconManager.startMonitoring(BeaconsData.ROOM_REGION);
             }
         }));
 
@@ -47,40 +41,18 @@ public class TakemeWithApplication extends Application {
             //Sample listener
             @Override
             public void onEnteredRegion(Region region, List<Beacon> list) {
-                if (region.equals(BeaconsData.ROOM1_REGION)) {
-                    // Region 1 shows a toast
-                    Toast.makeText(getApplicationContext(), "Region 1 entered", Toast.LENGTH_LONG).show();
-                } else if (region.equals(BeaconsData.ROOM2_REGION)) {
-                    // Region 2 shows a notification
-                    showNotification("New Region!", "Region 2 Entered");
+                if (region.equals(BeaconsData.ROOM_REGION)) {
+                    Toast.makeText(getApplicationContext(), "House entered", Toast.LENGTH_LONG).show();
                 }
             }
             @Override
             public void onExitedRegion(Region region) {
                 // could add an "exit" notification too if you want (-:
-                Toast.makeText(getApplicationContext(), "Region 1 exited", Toast.LENGTH_LONG).show();
-
+                if (region.equals(BeaconsData.ROOM_REGION)) {
+                    Toast.makeText(getApplicationContext(), "House exited", Toast.LENGTH_LONG).show();
+                }
             }
         });
-    }
-
-    public void showNotification(String title, String message) {
-        Intent notifyIntent = new Intent(this, MainActivity.class);
-        notifyIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivities(this, 0,
-                new Intent[] { notifyIntent }, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
-                .setSmallIcon(android.R.drawable.ic_dialog_info)
-                .setContentTitle(title)
-                .setContentText(message)
-                .setAutoCancel(true)
-                .setContentIntent(pendingIntent);
-
-        NotificationManager mNotifyMgr =
-                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        mNotifyMgr.notify(001, builder.build());
-
     }
 
 }
