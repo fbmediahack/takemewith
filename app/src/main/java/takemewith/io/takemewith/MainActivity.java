13dd074b76a44b21b1391ea16fcb04ac651351a2
Vladimir Kislicins <vladimir@seenit.io>
2016-09-24 11:18:04 +0100

    initial commit

---
+++ app/src/main/java/takemewith/io/takemewith/MainActivity.java
@@ -0,0 +1,13 @@
+package takemewith.io.takemewith;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
